98ba88aca0cb502a4a1fe32eda7aba869316844d
Mitchell <ymvang89@gmail.com>
2020-05-31 15:41:34 -0500

    Font and dropdown

--- src/App.js
+++ src/App.js
@@ -1,40 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Dropdown } from "semantic-ui-react";
+
 const BACKGROUND = [
-  require('./images/bg1.jpg'),
-  require('./images/bg2.jpg'),
-  require('./images/bg3.jpg')
+  require("./images/bg1.jpg"),
+  require("./images/bg2.jpg"),
+  require("./images/bg3.jpg"),
 ];
-let currentImage = '';
+let currentImage = "";
+
+const movieGenres = [
+  {
+    key: "Action",
+    text: "Action",
+    value: "Action",
+  },
+  {
+    key: "Adventure",
+    text: "Adventure",
+    value: "Adventure",
+  },
+  {
+    key: "Comedy",
+    text: "Comedy",
+    value: "Comedy",
+  },
+  {
+    key: "Documentary",
+    text: "Documentary",
+    value: "Documentary",
+  },
+  {
+    key: "Drama",
+    text: "Drama",
+    value: "Drama",
+  },
+  {
+    key: "Fantasy",
+    text: "Fantasy",
+    value: "Fantasy",
+  },
+  {
+    key: "Horror",
+    text: "Horror",
+    value: "Horror",
+  },
+  {
+    key: "Musical",
+    text: "Musical",
+    value: "Musical",
+  },
+  {
+    key: "Romance",
+    text: "Romance",
+    value: "Romance",
+  },
+  {
+    key: "Sci-fi",
+    text: "Sci-fi",
+    value: "Sci-fi",
+  },
+];
 
 function BackgroundRefresh() {
-    BACKGROUND.forEach((image, index) => {
-      if (currentImage === BACKGROUND[index]) {
-        currentImage = BACKGROUND[index++];
-      } else {
-        currentImage = BACKGROUND[0];
-      }
-    })
-    console.log(currentImage)
+  BACKGROUND.forEach((image, index) => {
+    if (currentImage === BACKGROUND[index]) {
+      currentImage = BACKGROUND[index++];
+    } else {
+      currentImage = BACKGROUND[0];
+    }
+  });
+  return currentImage;
 }
 
 function App() {
-
-  setInterval(function() {BackgroundRefresh()}, 3000);
+  //setInterval(function() {BackgroundRefresh()}, 3000);
 
   return (
     <div className="App">
-      <div className="Background" style={{backgroundImage: "url(" + currentImage + ")"}}> </div>
+      <div
+        className="Background"
+        style={{ backgroundImage: "url(" + BackgroundRefresh() + ")" }}
+      >
+        {" "}
+      </div>
       <header className="App-header">
-      
         <h1>OnPointFlix</h1>
-        <p>
-          Edit <code>src/App.js</code> to make changes bro.
-        </p>
-        <a>
-          Learn React
-        </a>
+
+        <p>What to watch?</p>
+
+        <button class="ui primary button">FLIX ME</button>
+
+        <div className="Dropdown">
+          <Dropdown
+            clearable
+            placeholder="Genre"
+            options={movieGenres}
+            selection
+          />
+        </div>
       </header>
     </div>
   );
