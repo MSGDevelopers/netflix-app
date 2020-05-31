734adc7e0b40f3e39dd3a64a7bf761bd0dc55a59
Mitchell <ymvang89@gmail.com>
2020-05-30 23:24:27 -0500

    Background Image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const BACKGROUND = [
+  require('./images/bg1.jpg'),
+  require('./images/bg2.jpg'),
+  require('./images/bg3.jpg')
+];
+let currentImage = '';
+
+function BackgroundRefresh() {
+    BACKGROUND.forEach((image, index) => {
+      if (currentImage === BACKGROUND[index]) {
+        currentImage = BACKGROUND[index++];
+      } else {
+        currentImage = BACKGROUND[0];
+      }
+    })
+    console.log(currentImage)
+}
 
 function App() {
+
+  setInterval(function() {BackgroundRefresh()}, 3000);
+
   return (
     <div className="App">
+      <div className="Background" style={{backgroundImage: "url(" + currentImage + ")"}}> </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        <h1>OnPointFlix</h1>
         <p>
           Edit <code>src/App.js</code> to make changes bro.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a>
           Learn React
         </a>
       </header>
